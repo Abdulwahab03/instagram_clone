bde358b782415cd865fa554c3c2231d31e17a5fd
Wahab Saheed Oluwatoyin <swahab947@gmail.com>
2023-03-01 16:01:45 +0100

    initial commit

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,3 +1,21 @@
 
 
-const webScreenSize = 700;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:instagram/screen/add_post.dart';
+import 'package:instagram/screen/feed_screen.dart';
+import 'package:instagram/screen/profile_screen.dart';
+import 'package:instagram/screen/search_screen.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
+
